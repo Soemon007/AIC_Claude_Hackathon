cdb4d76ed83db7756b26082e17e918a427380ebe
Soemon007 <rehanmallik31@gmail.com>
2026-05-05 16:53:57 +0530

    Delete frontend directory

--- frontend/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:8000";
-
-export const analyzeIngredients = async (payload) => {
-  const res = await axios.post(`${API_BASE_URL}/analyze`, payload);
-  return res.data;
-};
